b0876d7e2318515bd9e78464ba31e9b4e34a65e6
Josh Hepworth <jrhepworth12@gmail.com>
2018-10-22 12:53:17 -0500

    seed route

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -31,6 +31,170 @@ router.get('/new', (req, res) => {
 		}
 	})
 })
+
+
+router.get('/seed', (req, res) => {
+	Artist.create(
+		[
+		  {
+		    name: 'Bo Jackson',
+		    city: 'Chicago',
+		    yearsExp: 20,
+		    // work: [tats],
+		    // clients: [client],
+		    // appointments: [appointments]
+		    
+		    // tattoos: [tatoos],
+		    // appointments: [appointments]
+		  },{
+		    name: 'Brian Esparza',
+		    city: 'Chicago',
+		    yearsExp: 6,
+		    // work: [tats],
+		    // clients: [client],
+		    // appointments: [appointments]
+		    
+		    // tattoos: [tatoos],
+		    // appointments: [appointments]
+		  },{
+		    name: 'Luke Xan',
+		    city: 'Denver',
+		    yearsExp: 15,
+		    // work: [tats],
+		    // clients: [client],
+		    // appointments: [appointments]
+		    
+		    // tattoos: [tatoos],
+		    // appointments: [appointments]
+		  },{
+		    name: 'Bill Nye the Tatto Guy',
+		    city: 'Austin',
+		    yearsExp: 30,
+		    // work: [tats],
+		    // clients: [client],
+		    // appointments: [appointments]
+		    
+		    // tattoos: [tatoos],
+		    // appointments: [appointments]
+		  },{
+		    name: 'Joe CRUD',
+		    city: 'Dallas',
+		    yearsExp: 12,
+		    // work: [tats],
+		    // clients: [client],
+		    // appointments: [appointments]
+		    
+		    // tattoos: [tatoos],
+		    // appointments: [appointments]
+		  },{
+		    name: 'Bob Lee Swagger',
+		    city: 'Austin',
+		    yearsExp: 4,
+		    // work: [tats],
+		    // clients: [client],
+		    // appointments: [appointments]
+		    
+		    // tattoos: [tatoos],
+		    // appointments: [appointments]
+		  },{
+		    name: 'Katrina Zey',
+		    city: 'Denver',
+		    yearsExp: 17,
+		    // work: [tats],
+		    // clients: [client],
+		    // appointments: [appointments]
+		    
+		    // tattoos: [tatoos],
+		    // appointments: [appointments]
+		  },{
+		    name: 'Zoey Foyd',
+		    city: 'Chicago',
+		    yearsExp: 8,
+		    // work: [tats],
+		    // clients: [client],
+		    // appointments: [appointments]
+		    
+		    // tattoos: [tatoos],
+		    // appointments: [appointments]
+		  },{
+		    name: 'Jill Stein',
+		    city: 'Austin',
+		    yearsExp: 24,
+		    // work: [tats],
+		    // clients: [client],
+		    // appointments: [appointments]
+		    
+		    // tattoos: [tatoos],
+		    // appointments: [appointments]
+		  },{
+		    name: 'Bo Burnham',
+		    city: 'Denver',
+		    yearsExp: 13,
+		    // work: [tats],
+		    // clients: [client],
+		    // appointments: [appointments]
+		    
+		    // tattoos: [tatoos],
+		    // appointments: [appointments]
+		  },{
+		    name: 'Sal Rosenberg',
+		    city: 'Chicago',
+		    yearsExp: 4,
+		    // work: [tats],
+		    // clients: [client],
+		    // appointments: [appointments]
+		    
+		    // tattoos: [tatoos],
+		    // appointments: [appointments]
+		  },{
+		    name: 'Morgan Freeman',
+		    city: 'Chicago',
+		    yearsExp: 40,
+		    // work: [tats],
+		    // clients: [client],
+		    // appointments: [appointments]
+		    
+		    // tattoos: [tatoos],
+		    // appointments: [appointments]
+		  },{
+		    name: 'Angel Swanson',
+		    city: 'San Francisco',
+		    yearsExp: 27,
+		    // work: [tats],
+		    // clients: [client],
+		    // appointments: [appointments]
+		    
+		    // tattoos: [tatoos],
+		    // appointments: [appointments]
+		  },{
+		    name: 'Filip Tatarnik',
+		    city: 'Chicago',
+		    yearsExp: 0,
+		    // work: [tats],
+		    // clients: [client],
+		    // appointments: [appointments]
+		    
+		    // tattoos: [tatoos],
+		    // appointments: [appointments]
+		  },{
+		    name: 'Josh Hepworth',
+		    city: 'Chicago',
+		    yearsExp: 0,
+		    // work: [tats],
+		    // clients: [client],
+		    // appointments: [appointments]
+		    
+		    // tattoos: [tatoos],
+		    // appointments: [appointments]
+		  }
+		],
+		(err, createdArtists) => {
+			console.log(createdArtists)
+			res.send(createdArtists)
+		}
+	)
+})
+
 // ******************** SHOW ROUTE ******************** //
 router.get('/:id', (req, res) => {
 	Artist.findById(req.params.id,
