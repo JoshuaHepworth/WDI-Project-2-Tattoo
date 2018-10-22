f6bc86009afd25daceb5c268413dac171f824aab
Filip Tatarnik <ftatarnik@gmail.com>
2018-10-22 10:34:32 -0500

    Artist Index Works

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -4,4 +4,29 @@ const router = express.Router();
 
 const Artist = require('../models/artists');
 
+// ******************** INDEX ROUTE ******************** //
+router.get('/', (req, res) => {
+	Artist.find({},
+		(err, foundArtists) => {
+			if(err){console.log('--------ERROR--------', err);}
+			else{
+				console.log('----------FOUND ARTISTS--------', foundArtists);
+				res.render('../views/artists/index.ejs')
+			}
+		}
+	)
+})
+// ******************** NEW ROUTE ******************** //
+
+// ******************** CREATE ROUTE ******************** //
+
+// ******************** SHOW ROUTE ******************** //
+
+// ******************** DESTROY ROUTE ******************** //
+
+// ******************** EDIT ROUTE ******************** //
+
+// ******************** UPDATE ROUTE ******************** //
+
+
 module.exports = router;
