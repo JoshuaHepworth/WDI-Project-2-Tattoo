88b0f8e1dff7c320a0800a0b983c496871fcd55b
Josh Hepworth <jrhepworth12@gmail.com>
2018-10-25 12:34:27 -0500

    changed homepage to have pictures

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -14,6 +14,7 @@ router.get('/', (req, res) => {
 				let uniqueCity = []
 				let $uniqueCityTwo = uniqueCity
 				let artistCities = [];
+				let result = uniqueCity.filter(city => uniqueCity === allArtists);
 				for (let i = 0; i < allArtists.length; i++) {
 					 artistCities.push(allArtists[i].city)
 				};
@@ -47,7 +48,7 @@ router.get('/', (req, res) => {
 					client: foundClient,
 					artistId: foundArtist,
 					cities: uniqueCity,
-					$citiesTwo: $uniqueCityTwo
+					cityResult  : result
 				})
 			})
 		}
@@ -73,167 +74,182 @@ router.get('/new', (req, res) => {
 })
 })
 
-router.get('/seed', (req, res) => {
-	Artist.create(
-		[
-		  {
-		    name: 'Bo Jackson',
-		    city: 'Chicago',
-		    yearsExp: 20,
-		    // work: [tats],
-		    // clients: [client],
-		    // appointments: [appointments]
-		    
-		    // tattoos: [tatoos],
-		    // appointments: [appointments]
-		  },{
-		    name: 'Brian Esparza',
-		    city: 'Chicago',
-		    yearsExp: 6,
-		    // work: [tats],
-		    // clients: [client],
-		    // appointments: [appointments]
-		    
-		    // tattoos: [tatoos],
-		    // appointments: [appointments]
-		  },{
-		    name: 'Luke Xan',
-		    city: 'Denver',
-		    yearsExp: 15,
-		    // work: [tats],
-		    // clients: [client],
-		    // appointments: [appointments]
-		    
-		    // tattoos: [tatoos],
-		    // appointments: [appointments]
-		  },{
-		    name: 'Bill Nye the Tatto Guy',
-		    city: 'Austin',
-		    yearsExp: 30,
-		    // work: [tats],
-		    // clients: [client],
-		    // appointments: [appointments]
-		    
-		    // tattoos: [tatoos],
-		    // appointments: [appointments]
-		  },{
-		    name: 'Joe CRUD',
-		    city: 'Dallas',
-		    yearsExp: 12,
-		    // work: [tats],
-		    // clients: [client],
-		    // appointments: [appointments]
-		    
-		    // tattoos: [tatoos],
-		    // appointments: [appointments]
-		  },{
-		    name: 'Bob Lee Swagger',
-		    city: 'Austin',
-		    yearsExp: 4,
-		    // work: [tats],
-		    // clients: [client],
-		    // appointments: [appointments]
-		    
-		    // tattoos: [tatoos],
-		    // appointments: [appointments]
-		  },{
-		    name: 'Katrina Zey',
-		    city: 'Denver',
-		    yearsExp: 17,
-		    // work: [tats],
-		    // clients: [client],
-		    // appointments: [appointments]
-		    
-		    // tattoos: [tatoos],
-		    // appointments: [appointments]
-		  },{
-		    name: 'Zoey Foyd',
-		    city: 'Chicago',
-		    yearsExp: 8,
-		    // work: [tats],
-		    // clients: [client],
-		    // appointments: [appointments]
-		    
-		    // tattoos: [tatoos],
-		    // appointments: [appointments]
-		  },{
-		    name: 'Jill Stein',
-		    city: 'Austin',
-		    yearsExp: 24,
-		    // work: [tats],
-		    // clients: [client],
-		    // appointments: [appointments]
-		    
-		    // tattoos: [tatoos],
-		    // appointments: [appointments]
-		  },{
-		    name: 'Bo Burnham',
-		    city: 'Denver',
-		    yearsExp: 13,
-		    // work: [tats],
-		    // clients: [client],
-		    // appointments: [appointments]
-		    
-		    // tattoos: [tatoos],
-		    // appointments: [appointments]
-		  },{
-		    name: 'Sal Rosenberg',
-		    city: 'Chicago',
-		    yearsExp: 4,
-		    // work: [tats],
-		    // clients: [client],
-		    // appointments: [appointments]
-		    
-		    // tattoos: [tatoos],
-		    // appointments: [appointments]
-		  },{
-		    name: 'Morgan Freeman',
-		    city: 'Chicago',
-		    yearsExp: 40,
-		    // work: [tats],
-		    // clients: [client],
-		    // appointments: [appointments]
-		    
-		    // tattoos: [tatoos],
-		    // appointments: [appointments]
-		  },{
-		    name: 'Angel Swanson',
-		    city: 'San Francisco',
-		    yearsExp: 27,
-		    // work: [tats],
-		    // clients: [client],
-		    // appointments: [appointments]
-		    
-		    // tattoos: [tatoos],
-		    // appointments: [appointments]
-		  },{
-		    name: 'Filip Tatarnik',
-		    city: 'Chicago',
-		    yearsExp: 0,
-		    // work: [tats],
-		    // clients: [client],
-		    // appointments: [appointments]
-		    
-		    // tattoos: [tatoos],
-		    // appointments: [appointments]
-		  },{
-		    name: 'Josh Hepworth',
-		    city: 'Chicago',
-		    yearsExp: 0,
-		    // work: [tats],
-		    // clients: [client],
-		    // appointments: [appointments]
-		    
-		    // tattoos: [tatoos],
-		    // appointments: [appointments]
-		  }
-		],
-		(err, createdArtists) => {
-			console.log(createdArtists)
-			res.send(createdArtists)
-		}
-	)
-})
+// router.get('/seed', (req, res) => {
+//     Artist.create(
+//         [
+//           {
+//             name: 'Bo Jackson',
+//             city: 'Chicago',
+//             yearsExp: 20,
+//             urls: ['https://images.pexels.com/photos/807688/pexels-photo-807688.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350']
+//             // work: [tats],
+//             // clients: [client],
+//             // appointments: [appointments]
+            
+//             // tattoos: [tatoos],
+//             // appointments: [appointments]
+//           },{
+//             name: 'Brian Esparza',
+//             city: 'Chicago',
+//             yearsExp: 6,
+//             urls: ['https://images.pexels.com/photos/951001/pexels-photo-951001.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350']
+//             // work: [tats],
+//             // clients: [client],
+//             // appointments: [appointments]
+            
+//             // tattoos: [tatoos],
+//             // appointments: [appointments]
+//           },{
+//             name: 'Luke Xan',
+//             city: 'Denver',
+//             yearsExp: 15,
+//             urls: ['https://images.pexels.com/photos/428105/pexels-photo-428105.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350']
+//             // work: [tats],
+//             // clients: [client],
+//             // appointments: [appointments]
+            
+//             // tattoos: [tatoos],
+//             // appointments: [appointments]
+//           },{
+//             name: 'Bill Nye the Tatto Guy',
+//             city: 'Austin',
+//             yearsExp: 30,
+//             urls: ['https://images.pexels.com/photos/38870/pexels-photo-38870.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350']
+//             // work: [tats],
+//             // clients: [client],
+//             // appointments: [appointments]
+            
+//             // tattoos: [tatoos],
+//             // appointments: [appointments]
+//           },{
+//             name: 'Joe CRUD',
+//             city: 'Dallas',
+//             yearsExp: 12,
+//             urls: ['https://images.pexels.com/photos/163688/hiker-travel-trip-wander-163688.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350']
+//             // work: [tats],
+//             // clients: [client],
+//             // appointments: [appointments]
+            
+//             // tattoos: [tatoos],
+//             // appointments: [appointments]
+//           },{
+//             name: 'Bob Lee Swagger',
+//             city: 'Austin',
+//             yearsExp: 4,
+//             urls: ['https://images.pexels.com/photos/1338730/pexels-photo-1338730.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350']
+//             // work: [tats],
+//             // clients: [client],
+//             // appointments: [appointments]
+            
+//             // tattoos: [tatoos],
+//             // appointments: [appointments]
+//           },{
+//             name: 'Katrina Zey',
+//             city: 'Denver',
+//             yearsExp: 17,
+//             urls: ['https://images.pexels.com/photos/1327214/pexels-photo-1327214.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350']
+//             // work: [tats],
+//             // clients: [client],
+//             // appointments: [appointments]
+            
+//             // tattoos: [tatoos],
+//             // appointments: [appointments]
+//           },{
+//             name: 'Zoey Foyd',
+//             city: 'Chicago',
+//             yearsExp: 8,
+//             urls: ['https://images.pexels.com/photos/277320/pexels-photo-277320.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940']
+//             // work: [tats],
+//             // clients: [client],
+//             // appointments: [appointments]
+            
+//             // tattoos: [tatoos],
+//             // appointments: [appointments]
+//           },{
+//             name: 'Jill Stein',
+//             city: 'Austin',
+//             yearsExp: 24,
+//             urls: ['https://images.pexels.com/photos/838696/pexels-photo-838696.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350']
+//             // work: [tats],
+//             // clients: [client],
+//             // appointments: [appointments]
+            
+//             // tattoos: [tatoos],
+//             // appointments: [appointments]
+//           },{
+//             name: 'Bo Burnham',
+//             city: 'Denver',
+//             yearsExp: 13,
+//             urls: ['https://images.pexels.com/photos/248387/pexels-photo-248387.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350']
+//             // work: [tats],
+//             // clients: [client],
+//             // appointments: [appointments]
+            
+//             // tattoos: [tatoos],
+//             // appointments: [appointments]
+//           },{
+//             name: 'Sal Rosenberg',
+//             city: 'Chicago',
+//             yearsExp: 4,
+//             urls: ['https://images.pexels.com/photos/428320/pexels-photo-428320.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350']
+//             // work: [tats],
+//             // clients: [client],
+//             // appointments: [appointments]
+            
+//             // tattoos: [tatoos],
+//             // appointments: [appointments]
+//           },{
+//             name: 'Morgan Freeman',
+//             city: 'Chicago',
+//             yearsExp: 40,
+//             urls: ['https://images.pexels.com/photos/1913/iceland-person-arm-art.jpg?auto=compress&cs=tinysrgb&dpr=2&h=350']
+//             // work: [tats],
+//             // clients: [client],
+//             // appointments: [appointments]
+            
+//             // tattoos: [tatoos],
+//             // appointments: [appointments]
+//           },{
+//             name: 'Angel Swanson',
+//             city: 'San Francisco',
+//             yearsExp: 27,
+//             urls: ['https://images.pexels.com/photos/325152/pexels-photo-325152.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350']
+//             // work: [tats],
+//             // clients: [client],
+//             // appointments: [appointments]
+            
+//             // tattoos: [tatoos],
+//             // appointments: [appointments]
+//           },{
+//             name: 'Filip Tatarnik',
+//             city: 'Chicago',
+//             yearsExp: 0,
+//             urls: ['https://images.pexels.com/photos/48805/tattoo-foot-skin-black-and-white-48805.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350']
+//             // work: [tats],
+//             // clients: [client],
+//             // appointments: [appointments]
+            
+//             // tattoos: [tatoos],
+//             // appointments: [appointments]
+//           },{
+//             name: 'Josh Hepworth',
+//             city: 'Chicago',
+//             yearsExp: 0,
+//             urls: 'https://images.pexels.com/photos/1249214/pexels-photo-1249214.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+//             // work: [tats],
+//             // clients: [client],
+//             // appointments: [appointments]
+            
+//             // tattoos: [tatoos],
+//             // appointments: [appointments]
+//           }
+//         ],
+//         (err, createdArtists) => {
+//             console.log(createdArtists)
+//             res.send(createdArtists)
+//         }
+//     )
+// });
 
 // ******************** SHOW ROUTE ******************** //
 router.get('/:id', (req, res) => {
@@ -241,9 +257,6 @@ router.get('/:id', (req, res) => {
 		(err, foundArtist) => {
 			Artist.findOne({username: req.session.username}, (err, foundArtistId) => {
 				Client.findOne({username: req.session.username}, (err, foundClient) => {
-				if(err){console.log('----------ERROR---------', err);}
-				else{
-					console.log('---------------FOUND ARTIST----------', foundArtist);
 					res.render('artists/show.ejs',{
 						artist:foundArtist,
 						username: req.session.username,
@@ -252,11 +265,10 @@ router.get('/:id', (req, res) => {
 						client: foundClient,
 						artistId: foundArtistId
 					})
-				}
+				})
 			})
 		})
-	})
-});
+	});
 // ******************** SHOW ROUTE EMAIL ******************** //
 router.get('/:id/email', (req, res) => {
 	Artist.findById(req.params.id,
@@ -322,10 +334,10 @@ router.get('/:id/edit', (req, res) => {
 						artistId: foundArtist
 
 					})
-			}
+				}
+			})
+		})
 	})
-})
-})
 })
 router.put('/:id', (req, res) => {
 	Artist.findByIdAndUpdate(req.params.id, req.body,
